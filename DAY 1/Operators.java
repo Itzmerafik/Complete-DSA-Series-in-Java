226e5b1f8d6280b6ed4f1b158eb499bd940048ca
Mohammad Rafik <150713915+Itzmerafik@users.noreply.github.com>
2025-08-31 23:00:43 +0530

    Add files via upload

---
+++ DAY 1/Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+public static void main(String[] args) {
+    int A = 10;
+    int B = 5;
+    System.out.println("add = "+(A+B));
+    System.out.println("subtraction = "+ (A-B));
+     System.out.println("Mul = "+ (A*B));
+      System.out.println("Div = "+ (A/B));
+       System.out.println("Mod = "+ (A%B));
+}
+    
+}
