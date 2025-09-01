169c5a4463b6703d456ffdbb5af25775d81c0400
Mohammad Rafik <150713915+Itzmerafik@users.noreply.github.com>
2025-09-01 11:09:20 +0530

    Update Average.java

--- DAY 1/Average.java
+++ DAY 1/Average.java
@@ -1,3 +1,4 @@
+//To know  the average of the given inputs
 import java.util.Scanner;
 
 public class Practiceqsn1 {
@@ -10,3 +11,4 @@ public class Practiceqsn1 {
         System.out.println(Average);
     }
 }
+
